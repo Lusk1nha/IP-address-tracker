4a6ef625f05ab6857e78b5b7f1f8919062c54796
unknown <lucaspedro517@gmail.com>
2021-04-19 15:42:53 -0300

    finishing-api-consumer, error-warning, readme-update

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# IP-address-tracker-
-That shows the location on a map through an IP address.
+# IP-address-tracker
+
+That project shows the location on a map through an IP address.
+
+In this implementation, I used the IP Geolocation API to obtain the Country, City, ISP, and Latitude and Longitude through the user's IP.
+
+And with Latitude and Longitude, pass them through the Leaflet Map to show the exact location on the map. 
+
+[Geolocation API](https://geo.ipify.org/)
+[Leaflet Map](https://leafletjs.com/)
+
+[This is a challenge of FrontEnd Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0).
+
+## Result
+
+> ### See the result live [here](https://ip-address-tracker-lusk1nha.vercel.app/)
+
+Desktop Version -
+
+[![vercel.com](./public/assets/github-image-desktop.png)](https://ip-address-tracker-lusk1nha.vercel.app/)
+
+Mobile Version -
+
+[![vercel.com](./public/assets/github-image-mobile.png)](https://ip-address-tracker-lusk1nha.vercel.app/)
+
--- public/index.js
+++ public/index.js
@@ -1,4 +1,8 @@
-const buttonSend = document.querySelector('.ip-send')
+const userIP = document.querySelector('.input')
+const buttonSend = document.querySelector('form')
+
+const map = L.map('mapid')
+map.zoomControl.remove()
 
 const myIcon = L.icon({
   iconUrl: './assets/icon-location.svg',
@@ -6,70 +10,71 @@ const myIcon = L.icon({
   
 })
 
-const myMap = L.map('mapid').setView([34.08057, -118.07285], 17);
-myMap.zoomControl.remove()
-
-L.marker([34.08057, -118.07285], {icon: myIcon}).addTo(myMap)
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
-
-buttonSend.onclick = function() {
-  const userIP = document.querySelector('.ip-input')
+buttonSend.addEventListener('submit', event => {
   const IP = userIP.value
 
-  getLocation(IP)
-}
+  event.preventDefault()
+  return getLocation(IP)
 
-const getLocation = async (IP) => {
+})
+
+const getLocation = async (IP = '') => {
   const apiKEY = 'at_Yd7mTaxa7AviXSBV15z4hTyDfWKXh'
   const apiURL = `https://geo.ipify.org/api/v1?apiKey=${apiKEY}&ipAddress=${IP}`
   
-  const response = await fetch(apiURL)
-  const result = await response.json()
+  try {
+    let response = await fetch(apiURL)
+    let result = await response.json()
 
-  const location = result.location
-  const isp = result.isp
+    if ( result.ip === undefined ) return errorState()
 
-  console.log(location)
+    userIP.value = result.ip
+    userIP.placeholder = 'Search for any IP address or domain'
 
-  putIPInfos(IP, location, isp)
-  
+    return putIPInfos(result.ip, result.location, result.isp)
+
+  } catch (err) {
+    errorState()
+    return console.log('Request Failed', err)
+
+  }
 }
 
-const putIPInfos = (ipAddress, location, ispText) => {  
+const putIPInfos = (ipAddress, location, ispText) => {
+  userIP.classList.remove('input--state-error')
+
   const addressElement = document.querySelector('.address')
   addressElement.innerHTML = ipAddress
   
   const locationElement = document.querySelector('.location')
-  const locationValue = `${location.city}, ${location.country} ${location.postalCode}`
-  locationElement.innerHTML = locationValue
+  locationElement.innerHTML = `${location.city}, ${location.country} ${location.postalCode}`
 
   const timezoneElement = document.querySelector('.timezone')
-  const timezoneValue = `UTC ${location.timezone}`
-  timezoneElement.innerHTML = timezoneValue
+  timezoneElement.innerHTML = `UTC ${location.timezone}`
 
   const ispElement = document.querySelector('.isp')
   ispElement.innerHTML = ispText
 
   return renderMap(location.lat, location.lng)
-
 }
 
 const renderMap = (lat, lng) => {
   const newLocation = [lat, lng]
   
-  myMap.eachLayer((marker) => {
-    marker.remove()
-    
-  })
+  L.marker([34.08057, -118.07285], {icon: myIcon}).addTo(map)
   
-  myMap.setView(newLocation);
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  
-  return L.marker(newLocation, {icon: myIcon}).addTo(myMap)
+  map.eachLayer(marker => marker.remove())
+  map.setView(newLocation, 17);
 
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: false})
+  .addTo(map);
+  
+  return L.marker(newLocation, {icon: myIcon}).addTo(map)
+
+}
+
+const errorState = () => {
+  userIP.classList.add('input--state-error')
+  userIP.placeholder = "Please use a valid IP address"
+  userIP.value = ""
 }
